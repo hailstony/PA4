1a7ec3519cac96547a247aa8d8d05398b5a75ed9
Shengyang Shi <shengyang.shi@hotmail.com>
2016-02-18 19:37:27 -0800

    q2 passing all tests, but still having problem on score

--- multiAgents.py
+++ multiAgents.py
@@ -169,72 +169,73 @@ class MinimaxAgent(MultiAgentSearchAgent):
 
         result = self._maxvalue(gameState)
 
-        return result[1][1]
+        return result[1]
 
     # Max value expand
+    # The return value of the function will be a tuple
+    # (max_score, action_to_the_state_of_max_score)
     def _maxvalue(self, state, agent_index=0, depth=1):
 
+        # If the maximum depth is exceeded, return the evaluted score
+        # Since no action is taken, the place for the action will be None.
+        # However, this won't affect, because it won't be used.
+        if depth > self.depth:
+            return tuple((self.evaluationFunction(state), None))
+
+        # Get all possible actions for this agent,
+        # and generate a list of tuple of (successor_game_state, action_lead_to_this_successor)
         actions = state.getLegalActions(agent_index)
         successors = [tuple((state.generateSuccessor(agent_index, action), action)) for action in actions]
 
-        """
-        print "---------"
-        print "max"
-        print "depth: %d" % depth
-        print state
-        print actions
-        """
-
-        # The return value is a list of tuple of (minvalue, the state of this minvalue)
-        result = [tuple((self._minvalue(s[0], 1, depth)[0], s)) for s in successors]
+        # The return value is a list of tuple of (minvalue, action)
+        # The next agent for maxvalue will always be Ghost No.1
+        result = [tuple((self._minvalue(s[0], 1, depth)[0], s[1])) for s in successors]
 
         # If no action can be taken, this means a terminal condition occurs,
         # return score directly
         if len(result) is 0:
-            return tuple((state.getScore(), state))
+            return tuple((self.evaluationFunction(state), None))
 
-        max_heuristic = max(result, key=lambda x: x[0])[0]
-        result = [r for r in result if r[0] == max_heuristic]
+        max_heuristic = max(result, key=lambda x: x[0])[0]          # Max value
+        result = [r for r in result if r[0] == max_heuristic]       # Get the lists of the largest value
 
+        # If only one successor state has the largest value, return directly
+        # Else pick the first one that is not STOP
         if len(result) is 1:
             return result[0]
         else:
-            return filter(lambda x: x[1][1] is not Directions.STOP, result)[0]
+            return filter(lambda x: x[1] is not Directions.STOP, result)[0]
 
     # Min value expand
+    # The return value of the function will be a tuple
+    # (min_score, action_to_the_state_of_min_score)
     def _minvalue(self, state, agent_index, depth):
 
+        # Get all possible actions for this agent,
+        # and generate a list of tuple of (successor_game_state, action_lead_to_this_successor)
         actions = state.getLegalActions(agent_index)
         successors = [tuple((state.generateSuccessor(agent_index, action), action)) for action in actions]
 
-        """
-        print "---------"
-        print "min"
-        print "depth: %d" % depth
-        print state
-        print actions
-        """
-
+        # If this is not the last ghost, run minvalue again on the next ghost
+        # The return value is a list of tuple of (minvalue, action)
         if agent_index < state.getNumAgents() - 1:
-            result = [tuple((self._minvalue(s[0], agent_index + 1, depth)[0], s)) for s in successors]
+            result = [tuple((self._minvalue(s[0], agent_index + 1, depth)[0], s[1])) for s in successors]
+        # If this is the last ghost, run maxvalue on the pacman, add the depth
+        # The return value is a list of tuple of (maxvalue, action)
         else:
-            if depth >= self.depth:
-                return tuple((state.getScore(), state))
-            else:
-                result = [tuple((self._maxvalue(s[0], 0, depth + 1)[0], s)) for s in successors]
+            result = [tuple((self._maxvalue(s[0], 0, depth + 1)[0], s[1])) for s in successors]
 
-
+        # If a terminal condition occurs, return directly
         if len(result) is 0:
-            return tuple((state.getScore(), state))
+            return tuple((self.evaluationFunction(state), None))
 
-        min_heuristic = min(result, key=lambda x: x[0])[0]
-        result = [r for r in result if r[0] == min_heuristic]
-
+        min_heuristic = min(result, key=lambda x: x[0])[0]          # Min value
+        result = [r for r in result if r[0] == min_heuristic]       # The list of succesor state of min value
 
         if len(result) is 1:
             return result[0]
         else:
-            return filter(lambda x: x[1][1] is not Directions.STOP, result)[0]
+            return filter(lambda x: x[1] is not Directions.STOP, result)[0]
 
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
